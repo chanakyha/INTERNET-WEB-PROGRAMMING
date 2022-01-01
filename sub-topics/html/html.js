311f0f5df80b4896e0072166ee91d48ec3871392
chanakyha <vetrichanakyha2003@gmail.com>
2022-01-01 17:29:48 +0530

    Added Go to Top Button

--- sub-topics/html/html.js
+++ sub-topics/html/html.js
@@ -1,8 +1,12 @@
 function addPage(thePage) {
-    
     document.getElementById("main-container").innerHTML = "";
 
-    $("#main-container").load(thePage+".html");
-    
+    $("#main-container").load(thePage + ".html");
 
+    window.scrollTo(0,document.body.scrollHeight + 1200);
 }
+
+
+document.getElementById("go-up").onclick = function() {
+    window.scrollTo(0,0);
+};
